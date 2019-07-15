18a1df365472b5d774512c9bfe02b07bb2369d36
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-07-15 19:02:18 +0100

    add canCarry and carry methods to rocket class

---
+++ src/Rocket.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Rocket implements SpaceShip {
+
+  private int cost;
+  private int weight;
+  private int maxWeight;
+  private ArrayList<Item> cargo = new ArrayList<>();
+
+  public Rocket(int cost, int weight, int maxWeight) {
+    this.cost = cost;
+    this.weight = weight;
+    this.maxWeight = maxWeight;
+  }
+
+  public boolean launch() {
+    return true;
+  }
+
+  public boolean land() {
+    return true;
+  }
+
+  public boolean canCarry(Item item) {
+    return item.getWeight() <= maxWeight;
+  }
+
+  public void carry(Item item) {
+    cargo.add(item);
+    weight += item.getWeight();
+  }
+}
